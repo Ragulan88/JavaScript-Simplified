e076dc7c25da11d2153e0c286b9de243327064fc
Ragulan88 <rsuthaharan@gmail.com>
2021-12-28 14:18:36 +0000

    finishedv2

--- 44-45-modal/before/script.js
+++ 44-45-modal/before/script.js
@@ -23,11 +23,7 @@ openBtn.addEventListener("click", (e) => {
 // TODO: 4. Create a click event listener for the close-modal-btn that removes the class "open" from the modal
 // BONUS: Also remove the class "open" from the overlay
 
-closeBtn.addEventListener("click", (e) => {
-  // e.preventDefault()
-  modal.classList.remove("open")
-  overlay.classList.remove("open")
-})
+closeBtn.addEventListener("click", closeAll) // not here that since there was duplicate code, it was made into a function and then added in as second argument
 
 // BONUS: Add a click event listener to the overlay that removes the class "open" from the modal and the overlay
 
@@ -35,3 +31,8 @@ overlay.addEventListener("click", (e) => {
   modal.classList.remove("open")
   e.target.classList.remove("open") //nice, I am finally using e.target rather then overlay.classList
 })
+
+function closeAll() {
+  modal.classList.remove("open")
+  overlay.classList.remove("open")
+}
